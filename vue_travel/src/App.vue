5413de16fd39d59f72826577d1497e428098c680
cuiyunxiao <1527298532@qq.com>
2020-10-14 17:36:10 +0800

    头部

--- vue_travel/src/App.vue
+++ vue_travel/src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div >
-   hello
+  
     <router-view/>
   </div>
 </template>
---
+++ vue_travel/src/components/Home/pages/Header.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="header">
+  <div class="header-left">
+    <span class='iconfont'>&#xe648;</span>
+  </div>
+   <div class="header-search">
+     <span class='iconfont'>&#xe632;</span>
+     输入城市/景点/游玩主题
+   </div>
+    <div class="header-right">
+      广州
+     <span class='iconfont'>
+       &#xe64a;
+     </span>
+    </div>
+  </div>
+</template>
+
+<script >
+</script>
+
+<style scoped>
+  .header{
+    width: 100%;
+    height:.88rem;
+    background: #00bcd4;
+    color:#fff;
+    font-size: .36rem;
+    display: flex;
+  }
+</style>
