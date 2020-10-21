a1aaf69adf9343157866c8d4287fb18096b9c552
cuiyunxiao <1527298532@qq.com>
2020-10-21 17:29:50 +0800

    icons完善

---
+++ vue_travel/src/components/Home/pages/Icons.vue
@@ -0,0 +1,98 @@
+<template>
+  <div class="icons">
+      <div class="icons-item" v-for='item in iconsList' :key='item.id'>
+        <img :src="item.imgUlr" />
+        <p>{{item.title}}</p>
+      </div>
+
+
+  </div>
+</template>
+
+<script >
+  export default{
+    data(){
+      return{
+        iconsList:[
+          {
+            id:'01',
+            imgUlr:"http://s.qunarzz.com/homenode/images/touchheader/piao.png",
+            title:"景点门票"
+          },
+          {
+            id:'02',
+            imgUlr:"http://s.qunarzz.com/homenode/images/touchheader/hotel.png",
+             title:"酒店"
+          },
+          {
+            id:'03',
+            imgUlr:"http://s.qunarzz.com/homenode/images/touchheader/flight.png",
+             title:"机票"
+          },
+          {
+            id:'04',
+            imgUlr:"http://s.qunarzz.com/homenode/images/touchheader/train.png",
+             title:"火车票"
+          },
+          {
+            id:'05',
+            imgUlr:"http://s.qunarzz.com/homenode/images/touchheader/package.png",
+             title:"度假"
+          },
+          {
+            id:'06',
+            imgUlr:"https://picbed.qunarzz.com/1316dc82d1ce6259686d5a68880e5a9d.png",
+             title:"攻略"
+          },
+          {
+            id:'07',
+            imgUlr:"https://picbed.qunarzz.com/ae617a31e0bd5803d76918b817f6d942.png",
+             title:"自由行"
+          },
+          {
+            id:'08',
+            imgUlr:"https://picbed.qunarzz.com/a36d2288f19e54562338f4d8ef986288.png",
+             title:"低价机票"
+          },
+          {
+            id:'09',
+            imgUlr:"https://picbed.qunarzz.com/f5e5770393d759578962e53ee67798c8.png",
+             title:"海外酒店"
+          },
+          {
+            id:'10',
+            imgUlr:"https://picbed.qunarzz.com/01d2f57f920666364197a850dab859a8.png",
+             title:"民宿客栈"
+          },
+        ]
+      }
+    }
+  }
+
+</script>
+
+<style scoped>
+  .icons{
+    width: 100%;
+    overflow: hidden;
+  }
+  .icons-item{
+    width: 25%;
+    padding-bottom: 25%;
+    height: 0;
+    float: left;
+    }
+    .icons-item img{
+      width: 1.1rem;
+      height: 1.1rem;
+      display: block;
+      margin: 0 auto;
+      padding-top: .2rem;
+    }
+    .icons-item p{
+      font-size: .28rem;
+      text-align: center;
+      color:#212121;
+      margin-top: .1rem;
+    }
+</style>
