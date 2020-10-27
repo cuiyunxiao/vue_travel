f49aefcfc5c68dc753f8eab5d6e23510f053f15a
cuiyunxiao <1527298532@qq.com>
2020-10-27 17:31:21 +0800

    热门

--- vue_travel/src/components/Home/pages/Hot.vue
+++ vue_travel/src/components/Home/pages/Hot.vue
@@ -1,12 +1,48 @@
 <template>
-  <div>
-    
-    hot
+  <div class="hot">
+    <div class="hot-top">
+      <img src="https://simg1.qunarzz.com/piao/images/sub_nav/hot.png" />
+      <span>本周热门榜单</span>
+      <span class="hot-title">全部榜单 ></span>
+    </div>
+    <ul class="hot-list">
+      <li class="hot-item">
+        <img src=" ">
+        <p>故宫</p>
+        <p>
+          <span>￥20</span>起
+        </p>
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
+
 </script>
 
 <style scoped>
+  .hot{
+    margin-top:.2rem ;
+    background: #fff;
+    font-size:.28rem;
+  }
+  .hot-top{
+   padding: .2rem;
+  }
+  .hot-top img{
+    width:.3rem;
+    height:.3rem;
+  }
+  .hot-top span{
+    color: #212121;
+    font-size: .32rem;
+  }
+.hot-title{
+    position: absolute;
+    right: .2rem;
+    color:#616161;
+    font-size:.28rem;
+  }
+
 </style>
